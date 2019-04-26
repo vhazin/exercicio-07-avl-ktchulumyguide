6aaf9eaa11f90a450ddea994c32f05292a27e587
Andre (Jesus) <jesus@pop-os.localdomain>
2019-04-26 11:57:02 -0300

    updated

--- aopg_hjfcn.c
+++ aopg_hjfcn.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 struct node{
-	int height;
-	int data;
-	struct node * left;
-	struct node * right;
+  int height;
+  int data;
+  struct node * left;
+  struct node * right;
 };
 
 struct node * insert(struct node * root,int x);
@@ -14,165 +14,168 @@ struct node * rotateright(struct node * root);
 struct node * rotateleft(struct node *root);
 int height(struct node * root);
 void inorder(struct node * root);
-int findIndex(struct node * root, int number, int count);
+void findIndex(struct node * root, int number, int n);
 
 int main(){
 
-	struct node * root = NULL;
+  struct node * root = NULL;
 
-	int iterations;
+  int iterations;
 
-	scanf("%d", &iterations);
+  scanf("%d", &iterations);
 
-	for (int i = 0; i < iterations; i++){
-		int choice;
-		int number;
-		scanf("%d %d",&choice, &number);
+  for (int i = 0; i < iterations; i++){
+    int choice;
+    int number;
+    scanf("%d %d",&choice, &number);
 
-		if(choice == 1){
-			root = insert(root, number);
-		}else{
-			printf("%d\n", findIndex(root, number, 0));
-		}
+    if(choice == 1){
+      root = insert(root, number);
+    }else{
+      findIndex(root, number, iterations-1);
+    }
 
-	}
+  }
 
-	//inorder(root);
-
-	
-	return 0;
+  
+  return 0;
 }
 
 struct node * insert(struct node * root,int x){
 
-	if(root == NULL){
-		root = (struct node*)malloc(sizeof(struct node));
-		root->data = x;
-		root->left = NULL;
-		root->right  = NULL;
-	}
-	else{
-		if(x > root->data){
-			root->right = insert (root->right , x);
-			if(heights(root) == -2){
-				if(x > root->right->data){
-					root = rotateleft(root);
-				}else{
-					root->right = rotateright(root->right);
-					root = rotateleft(root);
-				}
-			}
-		}
-		else{
-			if(x < root->data){
-				root->left = insert(root->left , x);
-				if(heights(root) == 2){
-					if(x < root->left->data){
-						root = rotateright(root);
-					}else{
+  if(root == NULL){
+    root = (struct node*)malloc(sizeof(struct node));
+    root->data = x;
+    root->left = NULL;
+    root->right  = NULL;
+  }
+  else{
+    if(x > root->data){
+      root->right = insert (root->right , x);
+      if(heights(root) == -2){
+        if(x > root->right->data){
+          root = rotateleft(root);
+        }else{
+          root->right = rotateright(root->right);
+          root = rotateleft(root);
+        }
+      }
+    }
+    else{
+      if(x < root->data){
+        root->left = insert(root->left , x);
+        if(heights(root) == 2){
+          if(x < root->left->data){
+            root = rotateright(root);
+          }else{
 
-						root->left = rotateleft(root->left);
-						root = rotateright(root);
-					}
-				}
-			}
-		
-		root->height = height(root);
-		}
-	}
-	return(root);
+            root->left = rotateleft(root->left);
+            root = rotateright(root);
+          }
+        }
+      }
+    
+    root->height = height(root);
+    }
+  }
+  return(root);
 }
 
 int heights(struct node * root){
-	int lefth;
-	int righth;
-	if(root == NULL){
-		return(0);
-	}
+  int lefth;
+  int righth;
+  if(root == NULL){
+    return(0);
+  }
  
-	if(root->left==NULL){
-		lefth = 0;
-	}
-	else{
-		lefth = 1 + root->left->height;
-	}
+  if(root->left==NULL){
+    lefth = 0;
+  }
+  else{
+    lefth = 1 + root->left->height;
+  }
  
-	if(root->right == NULL){
-		righth = 0;
-	}
-	else{
-		righth = 1 + root->right->height;
-	}
+  if(root->right == NULL){
+    righth = 0;
+  }
+  else{
+    righth = 1 + root->right->height;
+  }
  
-	return(lefth - righth);
+  return(lefth - righth);
 }
 
 
 struct node * rotateright(struct node * root){
-	struct node * temp;
-	temp = root->left;
-	root->left = temp->right;
-	temp->right = root;
-	root->height = height(root);
-	temp->height = height(temp);
-	return(temp);
+  struct node * temp;
+  temp = root->left;
+  root->left = temp->right;
+  temp->right = root;
+  root->height = height(root);
+  temp->height = height(temp);
+  return temp;
 }
  
 struct node * rotateleft(struct node *root){
-	struct node * temp;
-	temp = root->right;
-	root->right = temp->left;
-	temp->left = root;
-	root->height = height(root);
-	temp->height = height(temp);
-	
-	return(temp);
+  struct node * temp;
+  temp = root->right;
+  root->right = temp->left;
+  temp->left = root;
+  root->height = height(root);
+  temp->height = height(temp);
+  
+  return temp;
 }
 
 int height(struct node * root)
 {
-	int lefth;
-	int righth;
-	if(root == NULL){
-		return(0);
-	}
-	
-	if(root->left == NULL){
-		lefth = 0;
-	}else{
-		lefth = 1 + root->left->height;
-	}
-		
-	if(root->right == NULL){
-		righth = 0;
-	}else{
-		righth = 1 + root->right->height;
-	}
-	
-	if(lefth > righth){
-		return(lefth);
-	}
-	
-	return(righth);
+  int lefth;
+  int righth;
+  if(root == NULL){
+    return 0;
+  }
+  
+  if(root->left == NULL){
+    lefth = 0;
+  }else{
+    lefth = 1 + root->left->height;
+  }
+    
+  if(root->right == NULL){
+    righth = 0;
+  }else{
+    righth = 1 + root->right->height;
+  }
+  
+  if(lefth > righth){
+    return(lefth);
+  }
+  
+  return(righth);
 }
 
-int findIndex(struct node * root, int number, int count){
-	if(root != NULL){
-		findIndex(root->left, number, count);
-		if(root->data == number){
-			return count;
-		}
-		printf("%d\n",root->data);
-		count++;
-		findIndex(root->right, number, count);
-	}
+void findIndex(struct node * root, int number, int size){
+  static int count = 0;
+  if(root != NULL){
+    findIndex(root->left, number, size);
+    count++;
+    if(root->data == number){
+      printf("%d \n", count - 1);
+      return;
+    }else if(count == size){
+      printf("Data tidal ada\n");
+    }
+    findIndex(root->right, number, size);
+
+  }
+  
 }
 
 void inorder(struct node * root){
-	//static int count = 0;
-	if(root != NULL){
-		inorder(root->left);
-		printf("%d\n",root->data);
-		inorder(root->right);
-	}
+  //static int count = 0;
+  if(root != NULL){
+    inorder(root->left);
+    printf("%d\n",root->data);
+    inorder(root->right);
+  }
 }
